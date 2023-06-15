ec552b774058ec255aa3b6c270d41f35a90cfe15
benudall98 <bu12@outlook.com>
2023-06-15 16:29:48 +0100

    implement add and edit employee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { EmployeesListComponent } from './employees-list/employees-list.component';
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeesListComponent
+    EmployeesListComponent,
+    EmployeeFormComponent,
+    AddEmployeeComponent,
+    EditEmployeeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule, 
+    ReactiveFormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
